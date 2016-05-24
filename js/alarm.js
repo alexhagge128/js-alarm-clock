871f3cedd2853224941715a3b0033002aef8c4a2
Alex Hagge and Kevin Deganos <kevin@>
2016-05-24 14:42:34 -0700

    added snooze button

--- js/alarm.js
+++ js/alarm.js
@@ -1,7 +1,19 @@
 exports.Alarm = function() {
 };
 
-exports.Alarm.prototype.currentTime = function() {
-  now = moment().format('HH:mm:ss a');
+exports.Alarm.prototype.getCurrentTime = function() {
+  now = moment().format('hh:mm:ss a');
   document.getElementById('timer').innerHTML = now;
+};
+
+exports.Alarm.prototype.setAlarm = function(alarmTime, currentTime) {
+  var timerHour = parseInt(alarmTime.slice(0, 2)) * 3600;
+  var timerMinute = parseInt(alarmTime.slice(3, 5)) * 60;
+  var timer = (timerHour + timerMinute) * 1000;
+
+  var currentHour = parseInt(currentTime.slice(0, 2)) * 3600;
+  var currentMinute = parseInt(currentTime.slice(3, 5)) * 60;
+  var current = (currentHour + currentMinute) * 1000;
+  
+  return timer - current;
 };
--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,8 +1,32 @@
 var Alarm = require('./../js/alarm.js').Alarm;
 
 $(document).ready(function(){
-    console.log(moment().format('LTS'));
-    var alarm = new Alarm();
+  $(".alarmState").hide();
 
-    setInterval(function(){alarm.currentTime()}, 1000);
+  var alarm = new Alarm();
+  setInterval(function(){alarm.getCurrentTime()}, 1000);
+
+  $("#formAlarm").submit(function(event){
+    event.preventDefault();
+
+    alarmTime = ($("#setAlarm").val());
+
+    var timer = alarm.setAlarm(alarmTime, moment().format("HH:mm"));
+
+    setTimeout(function(){
+      $(".alarmState").show();
+    }, timer);
+  });
+
+  $("#getUp").click(function(){
+    $(".alarmState").hide();
+  });
+
+  $("#snooze").click(function(){
+    $(".alarmState").hide();
+
+    setTimeout(function(){
+      $(".alarmState").show();
+    }, 5000);
+  });
 });
